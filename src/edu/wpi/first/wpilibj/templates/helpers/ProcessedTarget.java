5a9454e379aa8b71e980a3287592054b9b39f3e0
daboross <dabodialga@gmail.com>
2013-02-02 14:27:50 -0800

    Processed Target Class Add

---
+++ src/edu/wpi/first/wpilibj/templates/helpers/ProcessedTarget.java
@@ -0,0 +1,42 @@
+package edu.wpi.first.wpilibj.templates.helpers;
+
+import edu.wpi.first.wpilibj.image.ParticleAnalysisReport;
+
+/**
+ *
+ * @author daboross
+ */
+public class ProcessedTarget {
+
+    private int height;
+    private int width;
+    private int posX;
+    private int posY;
+
+    public ProcessedTarget(ParticleAnalysisReport p) {
+        height = p.boundingRectHeight;
+        width = p.boundingRectWidth;
+        posX = p.boundingRectTop;
+        posY = p.boundingRectLeft;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getX() {
+        return posX;
+    }
+
+    public int getY() {
+        return posY;
+    }
+
+    public ProcessedTarget findBiggestTarget(ProcessedTarget[] pts) {
+        int biggestArea=0;
+    }
+}

016dcf02a0d97efe7198a3e3b0a9b475d7b1078d
daboross <dabodialga@gmail.com>
2013-02-02 12:56:32 -0800

    Removed Pointlessness

--- src/edu/wpi/first/wpilibj/templates/Vst.java
+++ src/edu/wpi/first/wpilibj/templates/Vst.java
@@ -1,30 +1,4 @@
 package edu.wpi.first.wpilibj.templates;
 
-import edu.wpi.first.wpilibj.image.ColorImage;
-import edu.wpi.first.wpilibj.image.NIVisionException;
-
 public class Vst {
-
-    private static ColorImage currentCameraImage;
-
-    public static boolean setCCImage(ColorImage im) {
-        if (im != null) {
-            return false;
-        }
-        if (currentCameraImage != null) {
-            try {
-                currentCameraImage.free();
-            } catch (NIVisionException ex) {
-            }
-        }
-        currentCameraImage = im;
-        if (im != null) {
-            return true;
-        }
-        return false;
-    }
-
-    public static ColorImage getCCImage() {
-        return currentCameraImage;
-    }
 }

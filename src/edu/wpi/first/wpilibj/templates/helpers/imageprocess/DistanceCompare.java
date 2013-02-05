4e935a6d701250ebbf48389f840e3bee7fe120a6
Robotics <Robotics@10.94.250.244>
2013-02-04 19:53:50 -0800

    Created a class to compare reports.

---
+++ src/edu/wpi/first/wpilibj/templates/helpers/imageprocess/DistanceCompare.java
@@ -0,0 +1,9 @@
+package edu.wpi.first.wpilibj.templates.helpers.imageprocess;
+
+/**
+ * This Class is here to compare different reports. EG find the position of a
+ * target from a robot position. Or find the position of a robot from the
+ * target.
+ */
+public class DistanceCompare {
+}

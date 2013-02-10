79017cbcf0f965b4f66cf702bf31ad02aa64c932
Zachary Kotlarek <zach@kotlarek.com>
2013-02-10 02:34:38 -0800

    Run TestDrive by default, at least for the time being.

--- src/edu/wpi/first/wpilibj/templates/subsystems/TestMotors.java
+++ src/edu/wpi/first/wpilibj/templates/subsystems/TestMotors.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.templates.commands.TestDrive;
 import edu.wpi.first.wpilibj.templates.variablestores.VstM;
 
 public class TestMotors extends Subsystem {
@@ -14,19 +15,18 @@ public class TestMotors extends Subsystem {
     public TestMotors() {
         jag1 = new Jaguar(VstM.PWM.TEST_MOTOR_1);
         jag2 = new Jaguar(VstM.PWM.TEST_MOTOR_2);
+        drive = new RobotDrive(jag1, jag2);
     }
-
+    
     public void initDefaultCommand() {
-        setDefaultCommand(null);
+        setDefaultCommand(new TestDrive());
     }
 
     public void drive(Joystick stick) {
-        drive = new RobotDrive(jag1, jag2);
         drive.arcadeDrive(stick);
     }
 
     public void stop() {
-        drive.free();
         jag1.stopMotor();
         jag2.stopMotor();
     }

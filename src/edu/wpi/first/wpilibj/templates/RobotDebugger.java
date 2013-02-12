76f69085732d78ddfcbdbbd2fb7bc7044edebce3
daboross <dabodialga@gmail.com>
2013-02-11 19:14:35 -0800

    Moved Debugger

---
+++ src/edu/wpi/first/wpilibj/templates/RobotDebugger.java
@@ -0,0 +1,16 @@
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.templates.variablestores.VstM;
+
+public class RobotDebugger {
+
+    public static void push(String key, String value) {
+        if (VstM.Debug.CONSOLE) {
+            System.out.println("Debug Status: " + key + ": " + value);
+        }
+        if (VstM.Debug.DASHBOARD) {
+            SmartDashboard.putString("Debug:" + key, value);
+        }
+    }
+}

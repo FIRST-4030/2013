adf01a7f1bf04a355a3bd3111b624a343a8553bb
daboross <dabodialga@gmail.com>
2013-02-11 19:54:36 -0800

    Does Things Now

--- src/edu/wpi/first/wpilibj/templates/subsystems/GroundDrive.java
+++ src/edu/wpi/first/wpilibj/templates/subsystems/GroundDrive.java
@@ -1,7 +1,11 @@
 package edu.wpi.first.wpilibj.templates.subsystems;
 
 import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.templates.OI;
 import edu.wpi.first.wpilibj.templates.RobotDebugger;
 import edu.wpi.first.wpilibj.templates.commands.GroundDriveCommand;
 import edu.wpi.first.wpilibj.templates.variablestores.VstM;
@@ -12,10 +16,13 @@ import edu.wpi.first.wpilibj.templates.variablestores.VstM;
  */
 public class GroundDrive extends Subsystem {
 
-    private Jaguar leftMotor = new Jaguar(VstM.PWM.LEFT_MOTOR_PORT);
-    private Jaguar rightMotor = new Jaguar(VstM.PWM.RIGHT_MOTOR_PORT);
+    private static Jaguar leftMotor = new Jaguar(VstM.PWM.LEFT_MOTOR_PORT);
+    private static Jaguar rightMotor = new Jaguar(VstM.PWM.RIGHT_MOTOR_PORT);
+    private static RobotDrive roboDrive;
 
     public GroundDrive() {
+        roboDrive = new RobotDrive(leftMotor, rightMotor);
+        roboDrive.stopMotor();
     }
 
     public void initDefaultCommand() {
@@ -23,10 +30,11 @@ public class GroundDrive extends Subsystem {
     }
 
     public void driveWithXBox() {
-        
+        driveWithController(OI.getDriveJoystick());
     }
 
-    public void driveWithController() {
+    public void driveWithController(Joystick js) {
+        roboDrive.arcadeDrive(js);
     }
 
     public void statusPush() {

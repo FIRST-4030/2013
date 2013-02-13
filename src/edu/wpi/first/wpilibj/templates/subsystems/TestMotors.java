21a2d2687cf91bb2264f29b80909f5a0955a5db1
Robotics <Robotics@10.94.250.244>
2013-02-12 18:00:44 -0800

    Restore the TestMotors subsystem/command.
    Eventually this will need to use a different joystick/axis/button/etc. but this is still a useful base.

---
+++ src/edu/wpi/first/wpilibj/templates/subsystems/TestMotors.java
@@ -0,0 +1,33 @@
+package edu.wpi.first.wpilibj.templates.subsystems;
+
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.templates.commands.TestDrive;
+import edu.wpi.first.wpilibj.templates.variablestores.VstM;
+
+public class TestMotors extends Subsystem {
+
+    private Jaguar jag1, jag2;
+    private RobotDrive drive;
+
+    public TestMotors() {
+        jag1 = new Jaguar(VstM.PWM.TEST_MOTOR_1);
+        jag2 = new Jaguar(VstM.PWM.TEST_MOTOR_2);
+        drive = new RobotDrive(jag1, jag2);
+    }
+    
+    public void initDefaultCommand() {
+        setDefaultCommand(new TestDrive());
+    }
+
+    public void drive(Joystick stick) {
+        drive.arcadeDrive(stick);
+    }
+
+    public void stop() {
+        jag1.stopMotor();
+        jag2.stopMotor();
+    }
+}

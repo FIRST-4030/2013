cb7ed8d5a3fce72cb6ce165f7d0d223e8db5f646
daboross <dabodialga@gmail.com>
2013-02-12 17:03:11 -0800

    Resolved Conflicts

--- src/edu/wpi/first/wpilibj/templates/commands/RunClimber.java
+++ src/edu/wpi/first/wpilibj/templates/commands/RunClimber.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.wpi.first.wpilibj.templates.commands;
 
 import edu.wpi.first.wpilibj.templates.OI;
@@ -25,7 +21,6 @@ public class RunClimber extends CommandBase {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
         double climbSpeed = OI.getDriveJoystick().getRawAxis(VstM.Joysticks.Xbox.TRIGGERS);
-        
         if (climbSpeed < 0 && climber.isAtLowerLimit()) {
             climber.runLadder(climbSpeed);
         } else if (climbSpeed > 0 && climber.isAtUpperLimit()) {
--- src/edu/wpi/first/wpilibj/templates/subsystems/Climber.java
+++ src/edu/wpi/first/wpilibj/templates/subsystems/Climber.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.wpi.first.wpilibj.templates.subsystems;
 
 import edu.wpi.first.wpilibj.DigitalInput;
@@ -16,13 +12,13 @@ import edu.wpi.first.wpilibj.templates.variablestores.VstM;
  *
  * @author Robotics
  */
-public class Climber extends Subsystem {
+public class Climber extends Subsystem implements Debuggable {
 
-    //objects needed by climber
+    // Objects needed by climber
     private DigitalInput lowerLimit = new DigitalInput(VstM.Relays.LOWER_LIMIT_SWITCH);
     private DigitalInput upperLimit = new DigitalInput(VstM.Relays.UPPER_LIMIT_SWITCH);
     private Jaguar climber = new Jaguar(VstM.PWM.CLIMBER_MOTOR);
-    
+
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
     public void initDefaultCommand() {
@@ -37,22 +33,23 @@ public class Climber extends Subsystem {
         climber.stopMotor();
     }
 
-
     public boolean isAtLowerLimit() {
         /* 
          * this returns true if it's at the lower limit, assuming
          * that the switch is normally open.
-        */
+         */
         return lowerLimit.get(); //TODO: invert if normally closed
     }
-    
+
     public boolean isAtUpperLimit() {
         /* 
          * this returns true if it's at the upper limit, assuming
          * that the switch is normally open.
-        */
+         */
         return upperLimit.get(); //TODO: invert if normally closed
     }
-    
-    
+
+    public DebugInfo getStatus() {
+        return new DebugInfo("Climber:Status", "Climber Speed:" + climber.get());
+    }
 }

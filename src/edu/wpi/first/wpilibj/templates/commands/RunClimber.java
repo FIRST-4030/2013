bb1075c6a2dfbfca6430222f58f515547451cdc7
Robotics <Robotics@10.94.250.244>
2013-02-13 20:00:51 -0800

    Changed -Dabo

--- src/edu/wpi/first/wpilibj/templates/commands/RunClimber.java
+++ src/edu/wpi/first/wpilibj/templates/commands/RunClimber.java
@@ -23,17 +23,21 @@ public class RunClimber extends CommandBase {
         /*
          * Older Code, I Didn't Delete.
          if (!VstM.Climber.isRetracting) {
-         climbSpeed *= -1; //Won't This Just Reverse The Direction No Matter Which Way It Was Going In The First Place? Like if It Started Out Positive It Would Become Negative, and if it started out negative it would become positive.
+         climbSpeed *= -1; 
+         * //Won't This Just Reverse The Direction No Matter Which Way It Was Going In The First Place? 
+         * //Like if It Started Out Positive It Would Become Negative, and if it started out negative it would become positive.
          }
          */
-
-        if (!((VstM.Climber.climberState() == -1) == (climbSpeed < 0) || (VstM.Climber.climberState() == 1) == climbSpeed > 0)) {
-            climbSpeed = VstM.Climber.climberState();
+        if (climbSpeed != 0) {
+            if (((VstM.Climber.climberState() == -1) != (climbSpeed < 0))
+                    || ((VstM.Climber.climberState() == 1) != climbSpeed > 0)) {
+                climbSpeed *= VstM.Climber.climberState();
+            }
         }
         climber.runLadder(climbSpeed);
     }
+// Make this return true when this Command no longer needs to run execute()
 
-    // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
         return false;
     }

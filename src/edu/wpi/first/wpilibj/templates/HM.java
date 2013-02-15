f1a34032de709644060c23af2cff92adf9c9c7a4
Dabo Ross <dabodialga@gmail.com>
2013-02-15 14:33:22 -0800

    Added Helpfull Methods

---
+++ src/edu/wpi/first/wpilibj/templates/HM.java
@@ -0,0 +1,17 @@
+package edu.wpi.first.wpilibj.templates;
+
+/**
+ * This class holds various helpful methods, such as rounding.
+ *
+ * @author daboross
+ */
+public class HM {
+
+    public static double round(int place, double value) {
+        return ((double) ((int) (value * place))) / (place);
+    }
+
+    public static double round(double value) {
+        return round(100, value);
+    }
+}

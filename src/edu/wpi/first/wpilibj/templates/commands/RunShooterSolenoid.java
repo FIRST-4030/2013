878677c3c5c6770e1d4193b5b9c6796f7a78458d
Robotics <Robotics@10.94.250.244>
2013-02-14 21:28:01 -0800

    .

--- src/edu/wpi/first/wpilibj/templates/commands/RunShooterSolenoid.java
+++ src/edu/wpi/first/wpilibj/templates/commands/RunShooterSolenoid.java
@@ -4,6 +4,7 @@
  */
 package edu.wpi.first.wpilibj.templates.commands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.templates.debugging.RobotDebugger;
 
 /**
@@ -25,8 +26,11 @@ public class RunShooterSolenoid extends CommandBase {
     protected void execute() {
         if (oi.getDriveJoystick().getRawButton(1)) {
             shooterSolenoid.extend();
+            SmartDashboard.putBoolean("Shooter extending", true);
         } else {
             shooterSolenoid.retract();
+            SmartDashboard.putBoolean("Shooter extending", false);
+
         }
 
         RobotDebugger.push(shooterSolenoid);

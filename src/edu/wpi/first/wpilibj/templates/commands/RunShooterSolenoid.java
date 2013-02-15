ecfdef01a1a2a2b8cb1add8bd3322409a17d64cd
Robotics <Robotics@10.94.250.244>
2013-02-14 20:20:13 -0800

    .

--- src/edu/wpi/first/wpilibj/templates/commands/RunShooterSolenoid.java
+++ src/edu/wpi/first/wpilibj/templates/commands/RunShooterSolenoid.java
@@ -5,7 +5,6 @@
 package edu.wpi.first.wpilibj.templates.commands;
 
 import edu.wpi.first.wpilibj.templates.debugging.RobotDebugger;
-import edu.wpi.first.wpilibj.templates.variablestores.VstM;
 
 /**
  *

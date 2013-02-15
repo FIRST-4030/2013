320a2c31c3f1c0bc7dd79fe2904874d6d1a3c059
Robotics <Robotics@10.94.250.244>
2013-02-14 20:55:17 -0800

    Solenoid

--- src/edu/wpi/first/wpilibj/templates/subsystems/ShooterSolenoid.java
+++ src/edu/wpi/first/wpilibj/templates/subsystems/ShooterSolenoid.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.wpi.first.wpilibj.templates.subsystems;
 
 import edu.wpi.first.wpilibj.Solenoid;

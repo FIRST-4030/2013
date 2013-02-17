91227eb2562cd4bee473cd15a489c827329b3965
Dabo Ross <dabodialga@gmail.com>
2013-02-16 20:13:18 -0800

    Commit

--- src/edu/wpi/first/wpilibj/templates/commands/RunClimberArmSolenoids.java
+++ src/edu/wpi/first/wpilibj/templates/commands/RunClimberArmSolenoids.java
@@ -1,5 +1,9 @@
 package edu.wpi.first.wpilibj.templates.commands;
 
+import edu.wpi.first.wpilibj.templates.debugging.DebugOutput;
+import edu.wpi.first.wpilibj.templates.debugging.Debuggable;
+import edu.wpi.first.wpilibj.templates.debugging.InfoState;
+import edu.wpi.first.wpilibj.templates.debugging.RobotDebugger;
 import edu.wpi.first.wpilibj.templates.variablestores.DashboardStore;
 import edu.wpi.first.wpilibj.templates.vstj.VstJ;
 
@@ -7,7 +11,7 @@ import edu.wpi.first.wpilibj.templates.vstj.VstJ;
  *
  * @author daboross
  */
-public class RunClimberArmSolenoids extends CommandBase {
+public class RunClimberArmSolenoids extends CommandBase implements Debuggable {
 
     public RunClimberArmSolenoids() {
         DashboardStore.initClimber();
@@ -20,6 +24,7 @@ public class RunClimberArmSolenoids extends CommandBase {
     private boolean pressedLast = false;
     private boolean on = false;
     private boolean justStarted = true;
+    private String status = "No Status Set";
 
     protected void execute() {
         if (DashboardStore.getClimberEnabled()) {
@@ -30,19 +35,24 @@ public class RunClimberArmSolenoids extends CommandBase {
                 }
                 pressedLast = !pressedLast;
             }
-
             if (justStarted) {
+                status = "Just Started";
                 climberArmSolenoids.retract();
             } else {
                 if (on) {
+                    status = "ExtendSlow";
                     climberArmSolenoids.extendSlow();
                 } else {
+                    status = "release";
                     climberArmSolenoids.release();
                 }
             }
         } else {
+            status = "Not Enabled";
             climberArmSolenoids.retract();
         }
+        RobotDebugger.push(climberArmSolenoids);
+        RobotDebugger.push(this);
     }
 
     protected boolean isFinished() {
@@ -54,5 +64,9 @@ public class RunClimberArmSolenoids extends CommandBase {
 
     protected void interrupted() {
         end();
+    }
+
+    public DebugOutput getStatus() {
+        return new InfoState("RunClimberArmSolenoid", status);
     }
 }
--- src/edu/wpi/first/wpilibj/templates/subsystems/ClimberArmSolenoids.java
+++ src/edu/wpi/first/wpilibj/templates/subsystems/ClimberArmSolenoids.java
@@ -2,15 +2,17 @@ package edu.wpi.first.wpilibj.templates.subsystems;
 
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.templates.commands.RunClimber;
 import edu.wpi.first.wpilibj.templates.commands.RunClimberArmSolenoids;
+import edu.wpi.first.wpilibj.templates.debugging.DebugOutput;
+import edu.wpi.first.wpilibj.templates.debugging.Debuggable;
+import edu.wpi.first.wpilibj.templates.debugging.InfoState;
 import edu.wpi.first.wpilibj.templates.variablestores.VstM;
 
 /**
  *
  * @author daboross
  */
-public class ClimberArmSolenoids extends Subsystem {
+public final class ClimberArmSolenoids extends Subsystem implements Debuggable {
 
     private Solenoid solenoid1Side1, solenoid1Side2, solenoid2Side1, solenoid2Side2;
 
@@ -19,17 +21,20 @@ public class ClimberArmSolenoids extends Subsystem {
         solenoid1Side2 = new Solenoid(VstM.Solenoids.CLIMBER_ARM_1_SIDE_2);
         solenoid2Side1 = new Solenoid(VstM.Solenoids.CLIMBER_ARM_2_SIDE_1);
         solenoid2Side2 = new Solenoid(VstM.Solenoids.CLIMBER_ARM_2_SIDE_2);
+        release();
     }
 
     protected void initDefaultCommand() {
         setDefaultCommand(new RunClimberArmSolenoids());
     }
+    private String state = "None Set";
 
     public void extendFast() {
         solenoid1Side1.set(true);
         solenoid1Side2.set(false);
         solenoid2Side1.set(false);
         solenoid2Side2.set(true);
+        state = "Extend Fast";
     }
 
     public void extendSlow() {
@@ -37,6 +42,7 @@ public class ClimberArmSolenoids extends Subsystem {
         solenoid1Side2.set(false);
         solenoid2Side1.set(true);
         solenoid2Side2.set(false);
+        state = "Extend Slow";
     }
 
     public void retract() {
@@ -44,11 +50,18 @@ public class ClimberArmSolenoids extends Subsystem {
         solenoid1Side2.set(true);
         solenoid2Side1.set(false);
         solenoid2Side2.set(false);
+        state = "Retract Fast";
     }
-    public void release(){
+
+    public void release() {
         solenoid1Side1.set(false);
         solenoid1Side2.set(false);
         solenoid2Side1.set(false);
-        solenoid2Side2.set(false);
+        solenoid2Side2.set(true);
+        state = "Release";
+    }
+
+    public DebugOutput getStatus() {
+        return new InfoState("ClimberArmSolenoids", state);
     }
 }

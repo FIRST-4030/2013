f8a61eafea99a4195acd68bd0609869b9aa84d71
Dabo Ross <dabodialga@gmail.com>
2013-02-17 13:13:06 -0800

    Fixed OutOfMemoryError.

--- src/edu/wpi/first/wpilibj/templates/debugging/DebugLevel.java
+++ src/edu/wpi/first/wpilibj/templates/debugging/DebugLevel.java
@@ -23,27 +23,27 @@ public class DebugLevel {
      */
     public static final int HIGHEST = 5;
     /**
-     * 
+     *
      */
     public static final int HIGH = 4;
     /**
-     * 
+     *
      */
     public static final int MID = 3;
     /**
-     * 
+     *
      */
     public static final int LOW = 2;
     /**
-     * 
+     *
      */
     public static final int LOWEST = 1;
     /**
-     * 
+     *
      */
     public static final int NEVER = 0;
     /**
-     * 
+     *
      */
     public static int CURRENT = MID;
 }

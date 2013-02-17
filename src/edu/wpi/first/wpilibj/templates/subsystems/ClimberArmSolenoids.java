91a78842cd0ccb7204a3121f5d7e38dffad316d7
Dabo Ross <dabodialga@gmail.com>
2013-02-16 18:29:11 -0800

    Added ClimberArmSolenoids

---
+++ src/edu/wpi/first/wpilibj/templates/subsystems/ClimberArmSolenoids.java
@@ -0,0 +1,45 @@
+package edu.wpi.first.wpilibj.templates.subsystems;
+
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.templates.variablestores.VstM;
+
+/**
+ *
+ * @author daboross
+ */
+public class ClimberArmSolenoids extends Subsystem {
+
+    private Solenoid solenoid1Side1, solenoid1Side2, solenoid2Side1, solenoid2Side2;
+
+    public ClimberArmSolenoids() {
+        solenoid1Side1 = new Solenoid(VstM.Solenoids.CLIMBER_ARM_1_SIDE_1);
+        solenoid1Side2 = new Solenoid(VstM.Solenoids.CLIMBER_ARM_1_SIDE_2);
+        solenoid2Side1 = new Solenoid(VstM.Solenoids.CLIMBER_ARM_2_SIDE_1);
+        solenoid2Side2 = new Solenoid(VstM.Solenoids.CLIMBER_ARM_2_SIDE_2);
+    }
+
+    protected void initDefaultCommand() {
+    }
+
+    public void extendFast() {
+        solenoid1Side1.set(true);
+        solenoid1Side2.set(false);
+        solenoid2Side1.set(false);
+        solenoid2Side2.set(true);
+    }
+
+    public void extendSlow() {
+        solenoid1Side1.set(true);
+        solenoid1Side2.set(false);
+        solenoid2Side1.set(true);
+        solenoid2Side2.set(false);
+    }
+
+    public void retract() {
+        solenoid1Side1.set(false);
+        solenoid1Side2.set(true);
+        solenoid2Side1.set(false);
+        solenoid2Side2.set(false);
+    }
+}

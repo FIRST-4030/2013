d46b8aedd348518b999b8b56c5b1de32025cdb08
Dabo Ross <dabodialga@gmail.com>
2013-02-16 19:13:18 -0800

    New Class

---
+++ src/edu/wpi/first/wpilibj/templates/variablestores/DashboardStore.java
@@ -0,0 +1,15 @@
+package edu.wpi.first.wpilibj.templates.variablestores;
+
+/**
+ *
+ * @author daboross
+ */
+public class DashboardStore {
+
+    private static boolean isClimberEnabled;
+    public static void refresh() {
+    }
+    public static void getClimberEnabled(){
+        refresh();
+    }
+}

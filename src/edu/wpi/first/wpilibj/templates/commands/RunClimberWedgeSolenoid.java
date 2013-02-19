42e9e3651c679b69353ed07d793f585220f5d395
Dabo Ross <dabodialga@gmail.com>
2013-02-18 16:47:26 -0800

    Does Things Now

--- src/edu/wpi/first/wpilibj/templates/commands/RunClimberWedgeSolenoid.java
+++ src/edu/wpi/first/wpilibj/templates/commands/RunClimberWedgeSolenoid.java
@@ -1,12 +1,22 @@
 package edu.wpi.first.wpilibj.templates.commands;
 
 import edu.wpi.first.wpilibj.templates.dashboardrelations.DashboardStore;
+import edu.wpi.first.wpilibj.templates.debugging.DebugLevel;
+import edu.wpi.first.wpilibj.templates.debugging.DebugOutput;
+import edu.wpi.first.wpilibj.templates.debugging.Debuggable;
+import edu.wpi.first.wpilibj.templates.debugging.InfoState;
+import edu.wpi.first.wpilibj.templates.vstj.VstJ;
 
 /**
  *
  * @author daboross
  */
-public class RunClimberWedgeSolenoid extends CommandBase {
+public class RunClimberWedgeSolenoid extends CommandBase implements Debuggable {
+
+    /**
+     * -1 for retract, 0 for stay, 1 for extend.
+     */
+    private int state;
 
     public RunClimberWedgeSolenoid() {
         requires(climberWedgeSolenoid);
@@ -18,7 +28,18 @@ public class RunClimberWedgeSolenoid extends CommandBase {
 
     protected void execute() {
         if (DashboardStore.getIsClimberEnabled()) {
+            double controlValue = VstJ.getClimberWedgeSolenoidControlAxisValue();
+            if (controlValue < -0.5) {
+                state = -1;
+            } else if (controlValue > 0.5) {
+                state = 1;
+            } else {
+                state = 0;
+            }
+        } else {
+            climberWedgeSolenoid.stayPut();
         }
+        climberWedgeSolenoid.update();
     }
 
     protected boolean isFinished() {
@@ -30,5 +51,19 @@ public class RunClimberWedgeSolenoid extends CommandBase {
 
     protected void interrupted() {
         end();
+    }
+
+    public DebugOutput getStatus() {
+        return new InfoState("RunClimberWedge", getStateMessage(), DebugLevel.HIGH);
+    }
+
+    private String getStateMessage() {
+        if (state == -1) {
+            return "Retract";
+        } else if (state == 1) {
+            return "Extend";
+        } else {
+            return "Stay Put";
+        }
     }
 }

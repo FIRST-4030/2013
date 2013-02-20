fd3cfa0e7103825791413aa14324c4fae91a42c3
Zachary Kotlarek <zach@kotlarek.com>
2013-02-19 19:17:40 -0800

    Exit quickly to avoid side effects

--- src/edu/wpi/first/wpilibj/templates/commands/ReadSetCamera.java
+++ src/edu/wpi/first/wpilibj/templates/commands/ReadSetCamera.java
@@ -37,7 +37,8 @@ public class ReadSetCamera extends CommandBase {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        this.execute();
+        return true;
     }
 
     // Called once after isFinished returns true

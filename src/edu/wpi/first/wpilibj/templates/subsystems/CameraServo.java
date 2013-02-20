f4f45b93431c5c783f75fc61fcbd7307a1f67583
frc4030-git-team-lappy <frc4030-git-team-lappy@kotlarek.com>
2013-02-19 19:37:09 -0800

    updated camera angle

--- src/edu/wpi/first/wpilibj/templates/subsystems/CameraServo.java
+++ src/edu/wpi/first/wpilibj/templates/subsystems/CameraServo.java
@@ -18,7 +18,6 @@ import edu.wpi.first.wpilibj.templates.variablestores.VstM;
 public class CameraServo extends Subsystem implements Debuggable {
 
     private Servo cameraServo;
-    private int setLast = -50;
 
     public CameraServo() {
         System.out.println("CameraServo: Created");
@@ -31,17 +30,14 @@ public class CameraServo extends Subsystem implements Debuggable {
 
     public void turnLeft() {
         cameraServo.setAngle(150);
-        setLast = 0;
     }
 
     public void turnRight() {
         cameraServo.setAngle(45);
-        setLast = 90;
     }
 
     public DebugOutput getStatus() {
         DebugInfo[] infoList = new DebugInfo[3];
-        infoList[0] = new DebugStatus("CameraServo:Should", setLast, DebugLevel.MID);
         infoList[1] = new DebugStatus("CameraServo:Get", cameraServo.get(), DebugLevel.MID);
         infoList[2] = new DebugStatus("CameraServo:GetAngle", cameraServo.getAngle(), DebugLevel.MID);
         return new DebugInfoGroup(infoList);

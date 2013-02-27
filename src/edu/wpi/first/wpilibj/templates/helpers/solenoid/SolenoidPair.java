692daf902378b4ad68494fda910f0b5877f1f4be
Dabo Ross <dabodialga@gmail.com>
2013-02-26 19:09:11 -0800

    Added way to get the "state" of SolenoidPair as a String.

--- src/edu/wpi/first/wpilibj/templates/helpers/solenoid/SolenoidPair.java
+++ src/edu/wpi/first/wpilibj/templates/helpers/solenoid/SolenoidPair.java
@@ -11,6 +11,7 @@ public class SolenoidPair {
     private Solenoid extendingSolenoid;
     private Solenoid retractingSolenoid;
     private boolean extending = false;
+    private boolean isStateSet = false;
 
     public SolenoidPair(int extendingSolenoidPort, int retractingSolenoidPort) {
         extendingSolenoid = new Solenoid(extendingSolenoidPort);
@@ -21,15 +22,25 @@ public class SolenoidPair {
         retractingSolenoid.set(false);
         extendingSolenoid.set(true);
         extending = true;
+        isStateSet = true;
     }
 
     public void retract() {
         extendingSolenoid.set(false);
         retractingSolenoid.set(true);
         extending = false;
+        isStateSet = true;
     }
 
     public boolean isExtending() {
         return extending;
+    }
+
+    public String getState() {
+        if (isStateSet) {
+            return extending ? "Extending" : "Retracting";
+        } else {
+            return "Not Set";
+        }
     }
 }

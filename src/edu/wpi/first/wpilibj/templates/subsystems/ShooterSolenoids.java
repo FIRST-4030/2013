cea0a88a304154ad84db8a77ade027f3f69ef478
Dabo Ross <dabodialga@gmail.com>
2013-02-26 19:11:59 -0800

    Removed Unused Import

--- src/edu/wpi/first/wpilibj/templates/subsystems/ShooterSolenoids.java
+++ src/edu/wpi/first/wpilibj/templates/subsystems/ShooterSolenoids.java
@@ -3,7 +3,6 @@ package edu.wpi.first.wpilibj.templates.subsystems;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.templates.commands.RunShooterSolenoid;
 import edu.wpi.first.wpilibj.templates.debugging.DebugLevel;
-import edu.wpi.first.wpilibj.templates.debugging.DebugStatus;
 import edu.wpi.first.wpilibj.templates.debugging.DebugOutput;
 import edu.wpi.first.wpilibj.templates.debugging.Debuggable;
 import edu.wpi.first.wpilibj.templates.debugging.InfoState;
